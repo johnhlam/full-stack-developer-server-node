da53a6e74e683d2b2c07ae2ae6df44e178216818
johnlam7707 <johnlam7707@gmail.com>
2022-04-07 22:42:45 -0400

    Cleaned up TODOs

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import mongoose from 'mongoose';
 import helloController  from "./controllers/hello-controller.js";
 import tuitController from './controllers/tuit-controller.js';
 import userController   from "./controllers/user-controller.js";
-// TODO: How to upload some tweets into the DB by default?
-// TODO: Shouldn't the react client be the same? Can I just insert the JSON values into the DB?
+
 mongoose.connect('mongodb://localhost:27017/webdev');
 
 const app = express();
